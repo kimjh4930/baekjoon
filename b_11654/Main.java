e21fe051a0f5fa7cd1376db5b8f41c18fc9fbde3
Kim, Junha <kimjh4930@gmail.com>
2019-09-08 22:38:42 +0900

    11654. 아스키 코드

---
+++ b_11654/Main.java
@@ -0,0 +1,17 @@
+package b_11654;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		String input = scan.nextLine();
+		
+		System.out.println((int)input.charAt(0));
+		
+		scan.close();
+	}
+
+}
