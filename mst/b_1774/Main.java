ce3c7c322fba8806d7e78e3e3f40c3674a26eb3e
Kim, Junha <kimjh4930@gmail.com>
2019-10-12 23:50:28 +0900

    1774. 우주신과의 교감

---
+++ mst/b_1774/Main.java
@@ -0,0 +1,13 @@
+package mst.b_1774;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		scan.close();
+	}
+
+}
