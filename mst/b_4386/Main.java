1bba6b4767a336582d8eb870655fdb40a48d3e4e
Kim, Junha <kimjh4930@gmail.com>
2020-01-11 20:19:19 +0900

    4386. 별자리 만들기

--- mst/b_4386/Main.java
+++ mst/b_4386/Main.java
@@ -84,7 +84,7 @@ public class Main {
 	}
 	
 	static double calDist (Point p1, Point p2) {
-		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+		return Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
 	}
 
 }
